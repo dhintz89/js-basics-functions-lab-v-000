a07bf3a23cfeced40e040a83953b0c7ddd116ff1
Dan Hintz <dhintz@live.com>
2019-06-17 01:05:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ function distanceTravelledInFeet(start, destination) {
 function calculatesFarePrice(start, destination) {
   const distance = distanceTravelledInFeet(start, destination)
   switch (distance) {
-    case <= 400: return 0;
+    case <= 400: 
+      return 0;
     case > 400 && <= 2000: return distance * .02;
     case > 2000 && <= 2500: return 25;
     default: return "Cannot travel that far";
